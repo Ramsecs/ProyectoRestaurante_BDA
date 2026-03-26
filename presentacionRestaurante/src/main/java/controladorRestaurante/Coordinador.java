d6bdff8a3f6fb8b934f908206aa80f3e99e26e98
josmaraquintana <josmaraalexaq@gmail.com>
2026-03-25 22:02:34 -0700

    feat: regreso del coordinador
    no se me agrego, te lo regreso

--- presentacionRestaurante/src/main/java/controladorRestaurante/Coordinador.java
+++ presentacionRestaurante/src/main/java/controladorRestaurante/Coordinador.java
@@ -4,20 +4,50 @@
  */
 package controladorRestaurante;
 
+import objetosNegocioRestaurante.ClienteBO;
+import objetosNegocioRestaurante.IClienteBO;
+import pantallas.VentanaMenuAdmin;
+import pantallas.VentanaMenuCliente;
+
 /**
  *
  * @author RAMSES
  */
 public class Coordinador {
     
-//    private final IClienteDAO clienteDAO;
-//    private final IClienteBO clienteBO;
-//    
-//    public Coordinador(){
-//        this.clienteBO = new ClienteBO();
-//        this.clienteDAO = new ClienteDAO();
-//    }
+//Capas de negocio (BOs)
+    private final IClienteBO clienteBO;
     
+    //Ventanas que se usaran para la navegacion
+    private VentanaMenuAdmin ventana_menu_admin;
+    private VentanaMenuCliente ventana_menu_cliente;
+    
+    public Coordinador(){
+        this.clienteBO = new ClienteBO();
+    }
+     
+    /**
+     * Hacer visible la pantalla de menu admin, si esta no ha sido abierta entonces creamos una nueva
+     */
+    public void iniciarMenuAdmin(){
+        if (ventana_menu_admin == null) {
+            ventana_menu_admin = new VentanaMenuAdmin();
+        }
+        
+        ventana_menu_admin.setVisible(true);
+        
+    }
+
+    
+    
+    public void mostrarMenuCliente(){
+        if (ventana_menu_cliente == null) {
+            ventana_menu_cliente = new VentanaMenuCliente();
+        }
+        
+        ventana_menu_cliente.setVisible(true);
+        
+    }
     
     
 }
